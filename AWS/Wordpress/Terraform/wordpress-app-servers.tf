814ccef7b5f7bf0436ece20caeebb14703492eb0
Matt Gartman <mattgartman@gmail.com>
2016-10-11 19:20:15 -0400

    Updated app servers

--- AWS/Wordpress/Terraform/wordpress-app-servers.tf
+++ AWS/Wordpress/Terraform/wordpress-app-servers.tf
@@ -19,13 +19,14 @@ resource "aws_security_group" "wordpress_app" {
 	vpc_id = "${aws_vpc.wordpress_vpc.id}"
 }
 
+
 resource "aws_instance" "wordpress1" {
 	ami = "${var.aws_amazon_linux_ami}"
-	availability_zone = "us-east-1b"
+	availability_zone = "us-east-1c"
 	instance_type = "t2.micro"
 	key_name = "${var.aws_key_name}"
 	vpc_security_group_ids = ["${aws_security_group.wordpress_app.id}"]
-	subnet_id = "${aws_subnet.us-east-1b-public.id}"
+	subnet_id = "${aws_subnet.us-east-1c-public.id}"
 	tags {
 		Name = "wordpress1",
 		Env = "Test"
